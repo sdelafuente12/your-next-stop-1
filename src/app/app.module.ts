e40b258664c4b4767b6ca5c620da2eba0e7b9c2c
davidrpolk <davidrpolk@gmail.com>
2019-09-30 11:02:58 -0500

    switching repo destination to forked version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartScreenComponent
   ],
   imports: [
     BrowserModule,
