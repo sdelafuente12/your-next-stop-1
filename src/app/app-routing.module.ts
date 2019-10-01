cbd8a42bbdf110bd74d669cd118ba4ac9e25b29a
davidrpolk <davidrpolk@gmail.com>
2019-10-01 15:46:20 -0500

    smmal change to start screen format

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ExploreViewComponent } from './explore-view/explore-view.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 const routes: Routes = [
   { path: 'explore-view', component: ExploreViewComponent },
-  { path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
-  },
+  { path: '', component: StartScreenComponent },
 ];
 
 @NgModule({
