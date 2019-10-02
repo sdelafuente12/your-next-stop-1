546d394aa0817109df98d2041bd438c3a99610b7
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-02 10:27:39 -0500

    importing FormModule to test client-server connection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxBottomNavModule } from 'igniteui-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { ExploreViewComponent } from './explore-view/explore-view.component';
 import { RoutePageComponent } from './route-page/route-page.component';
 import { DetailsPageComponent } from './details-page/details-page.component';
 import { MapComponent } from './map/map.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { IgxBottomNavModule } from 'igniteui-angular';
 
 @NgModule({
   declarations: [
@@ -24,6 +26,8 @@ import { IgxBottomNavModule } from 'igniteui-angular';
     MapComponent
   ],
   imports: [
+    //CommonModule,
+    FormsModule,
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
