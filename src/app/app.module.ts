f8e669ade8f7a434ee08bf8c94101fb8b4645e4d
gmarinojr <garymarinojr@gmail.com>
2019-10-02 10:10:44 -0500

    added router module import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
