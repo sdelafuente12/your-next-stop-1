84df2c1a91eb74b9b69aed9dc9701a47f4458d7a
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-03 09:49:29 -0500

    added createTrip method to send input trip info to server ... commented out

--- src/app/route/route.component.ts
+++ src/app/route/route.component.ts
@@ -13,3 +13,33 @@ export class RouteComponent implements OnInit {
   }
 
 }
+
+
+// import { Component, OnInit } from '@angular/core';
+// import { TripsService } from '../services/trips.service';
+
+// @Component({
+//   selector: 'app-route',
+//   templateUrl: './route.component.html',
+//   styleUrls: ['./route.component.scss']
+// })
+// export class RouteComponent implements OnInit {
+
+//   origin = '';
+//   destination = '';
+
+//   constructor(private trips: TripsService) { }
+
+//   ngOnInit() {
+//   }
+
+//   createTrip() {
+//     console.log('TRIP NAME', this.origin);
+//     this.trips.createTrip({
+//       }).subscribe(trip => {
+//       console.log('user from user service', trip);
+//     })
+//   }
+
+// }
+
