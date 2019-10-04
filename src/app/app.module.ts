a3d1465c006fbfc121c9a6df00c536a70d7cd118
gmarinojr <garymarinojr@gmail.com>
2019-10-04 15:54:15 -0500

    added modules for forms and data entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,58 +16,65 @@ import { RouteComponent } from './route/route.component';
 import { DetailsComponent } from './details/details.component';
 import { MapComponent } from './map/map.component';
 import {
-	IgxCardModule,
-	IgxAvatarModule,
-	IgxIconModule,
-	IgxListModule,
-	IgxBottomNavModule,
-	IgxInputGroupModule,
-	IgxCarouselModule,
-	IgxButtonModule,
-	IgxRippleModule,
-	IgxTabsModule
+  IgxCardModule,
+  IgxAvatarModule,
+  IgxListModule,
+  IgxBottomNavModule,
+  IgxCarouselModule,
+  IgxTabsModule,
+  IgxIconModule,
+  IgxInputGroupModule,
+  IgxButtonModule,
+  IgxRippleModule,
+  IgxDatePickerModule,
+  IgxTimePickerModule,
+  IgxComboModule,
+  IgxSelectModule
 } from 'igniteui-angular';
 import { BottomNavRoutingComponent } from './bottomnav-routing/bottomnav-routing.component';
 import { AgmCoreModule } from '@agm/core';
 import { AgmDirectionModule } from 'agm-direction';
 // import { API_KEY } from '../../config.js';
 @NgModule({
-    declarations: [
-        AppComponent,
-        StartScreenComponent,
-        ExploreComponent,
-        RouteComponent,
-        DetailsComponent,
-        MapComponent,
-        TripsComponent,
-        FavoritesComponent,
-        BottomNavRoutingComponent,
-    ],
-    imports: [
-        FormsModule,
-        HttpClientModule,
-        BrowserModule,
-        AppRoutingModule,
-        ServiceWorkerModule.register('ngsw-worker.js', {
-            enabled: environment.production
-        }),
-        BrowserAnimationsModule,
-        IgxAvatarModule,
-        IgxIconModule,
-        IgxListModule,
-        IgxCardModule,
-        IgxBottomNavModule,
-        RouterModule,
-        AgmCoreModule.forRoot({}),
-        AgmDirectionModule,
-        IgxInputGroupModule,
-        IgxCarouselModule,
-        IgxButtonModule,
-        IgxRippleModule,
-        IgxTabsModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    StartScreenComponent,
+    ExploreComponent,
+    RouteComponent,
+    DetailsComponent,
+    MapComponent,
+    TripsComponent,
+    FavoritesComponent,
+    BottomNavRoutingComponent
+  ],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    BrowserAnimationsModule,
+    IgxAvatarModule,
+    IgxIconModule,
+    IgxListModule,
+    IgxCardModule,
+    IgxBottomNavModule,
+    IgxSelectModule,
+    RouterModule,
+    AgmCoreModule.forRoot({}),
+    AgmDirectionModule,
+    IgxInputGroupModule,
+    IgxCarouselModule,
+    IgxButtonModule,
+    IgxRippleModule,
+    IgxTabsModule,
+    IgxDatePickerModule,
+    IgxTimePickerModule,
+    IgxComboModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
