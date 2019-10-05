4ad7c710ad289b11246771270b0abf406742ce9f
gmarinojr <garymarinojr@gmail.com>
2019-10-05 16:24:08 -0500

    building chips

--- src/app/route/route.component.ts
+++ src/app/route/route.component.ts
@@ -1,19 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-route',
-//   templateUrl: './route.component.html',
-//   styleUrls: ['./route.component.scss']
-// })
-// export class RouteComponent implements OnInit {
-
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { TripsService } from '../services/trips.service';
 
@@ -38,28 +22,10 @@ export class RouteComponent implements OnInit {
   }
 
   public onSubmit() {
-    console.log('submit');
   }
 
   ngOnInit() {
-    this.getAllTrips();
   }
 
-  createTrip() {
-    // console.log('TRIP START', this.origin);
-    this.trips
-      .createTrip({
-        route: this.origin + ',' + this.destination
-      })
-      .subscribe(trip => {
-        console.log('trip was posted to the db', trip);
-      });
-  }
-
-  getAllTrips() {
-    // console.log('CLICKEDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD');
-    return this.trips.getAllTrips().subscribe(trips => {
-      console.log('trips are here', trips);
-    });
-  }
+  
 }
