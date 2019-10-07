0ed60459193388e78a69fbdaff2fe22c42c8c349
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-07 18:05:02 -0500

    added login with google hyperlink... will change to google sign-in button later

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../environments/environment';
+
 @Component({
   selector: 'app-start-screen',
   templateUrl: './start-screen.component.html',
   styleUrls: ['./start-screen.component.scss']
 })
 export class StartScreenComponent implements OnInit {
+
+  googleLoginUrl = `${environment.BASE_API_URL}/auth/google`;
 
   constructor() { }
 
