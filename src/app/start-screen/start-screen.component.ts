1ab571f2f18cf162d3aac1651a271056b58491b0
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-09 10:31:58 -0500

    added google sign-in route variable to start-screen component

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -9,6 +9,7 @@ import { environment } from '../../environments/environment';
 })
 export class StartScreenComponent implements OnInit {
 
+  currentUser = localStorage.getItem('username');
   googleLoginUrl = `${environment.BASE_API_URL}/auth/google`;
 
   constructor() { }
