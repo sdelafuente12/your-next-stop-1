c62700bf682bec46a5367349a962589f403c82df
gmarinojr <garymarinojr@gmail.com>
2019-10-09 14:55:53 -0500

    converted favorites to places

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { ExploreComponent } from './explore/explore.component';
 import { StartScreenComponent } from './start-screen/start-screen.component';
 import { TripsComponent } from './trips/trips.component';
-import { FavoritesComponent } from './favorites/favorites.component';
+import { PlacesComponent } from './places/places.component';
 import { ResultsComponent } from './results/results.component';
 import { DetailsComponent } from './details/details.component';
 import { StatsComponent } from './stats/stats.component';
@@ -12,6 +12,7 @@ import {
   BottomNavRoutingComponent,
 } from './bottomnav-routing/bottomnav-routing.component';
 import { NavDrawerComponent } from './nav-drawer/nav-drawer.component';
+import { ɵPLATFORM_SERVER_ID } from '@angular/common';
 
 const routes: Routes = [
   {
@@ -25,7 +26,7 @@ const routes: Routes = [
   { path: 'explore', component: ExploreComponent },
   { path: 'route', component: RouteComponent },
   { path: 'trips', component: TripsComponent },
-  { path: 'favorites', component: FavoritesComponent },
+  { path: 'places', component: PlacesComponent },
   { path: 'explore/results', component: ResultsComponent },
   { path: 'details', component: DetailsComponent },
   { path: 'stats', component: StatsComponent },
