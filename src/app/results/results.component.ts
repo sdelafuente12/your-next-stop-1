25fc2326a2584f235add5afe7dd6be0f21dc9a73
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-11 10:42:36 -0500

    dynamically rendering all nearby palces

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { LocationService } from '../services/location.service'
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent implements OnInit {
 
-  constructor() { }
+export class ResultsComponent implements OnInit {
+  @Output() placesLoaded = new EventEmitter<string>();
+  currentUser = localStorage.getItem('userId');
+  public allPlaces = [];
+  allPlacesSubscription;
+  constructor(private locationService: LocationService) { }
 
   ngOnInit() {
-  }    
+    this.loadPlaces();
+  }
+
+  loadPlaces() {
+    return this.locationService.getCurrentPosition()
+    .subscribe(loc => {
+      console.log('LOCATION NATION', loc);
+      this.allPlacesSubscription = this.locationService.getAllNearbyPlaces(loc)
+      .subscribe(place => {
+        this.allPlaces.push(place)
+        console.log('ALL PLACES', this.allPlaces[0])
+      });
+    })
+  }
 
 }
