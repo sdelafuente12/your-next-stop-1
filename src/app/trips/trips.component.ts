f1493c210742922df8c77afadb210c69037cb6a7
sdelafuente12 <s.delafuente831@gmail.com>
2019-10-12 19:44:14 -0500

    added type to response

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -34,7 +34,7 @@ export class TripsComponent implements OnInit {
 
   getAllTrips() {
     return this.trips.getAllTrips(this.currentUser)
-    .subscribe(response => {
+    .subscribe((response: Array<any>): void => {
       console.log('USERS TRIPS RESPONSE', response);
       response.forEach(element => {
         if(element[0].status === 'current'){
