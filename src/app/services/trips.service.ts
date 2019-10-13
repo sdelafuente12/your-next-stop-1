aa9b7b673e65e15283c1b037ee1b99dd7d5b3df7
raphaelkhan8 <raphaelkhan8@gmail.com>
2019-10-13 10:48:40 -0500

    added upvote functionality

--- src/app/services/trips.service.ts
+++ src/app/services/trips.service.ts
@@ -10,12 +10,18 @@ import { environment } from '../../environments/environment';
 export class TripsService {
 
   private getAllTripsEndpoint = `${environment.BASE_API_URL}/getAllUsersTrips`;
+  private upvoteInterestEndpoint = `${environment.BASE_API_URL}/likedInterest`;
 
   constructor(private http: HttpClient) { }
 
   getAllTrips(user) {
     console.log('ENDGAME', this.getAllTripsEndpoint);
     return this.http.get(`${this.getAllTripsEndpoint}?id=${user}`);
+  }
+
+  upvoteInterest(upvotedPlace, userId) {
+    console.log('UPVOTE SERVICE', this.upvoteInterestEndpoint);
+    return this.http.post(this.upvoteInterestEndpoint, { interest: upvotedPlace.interest, userId: userId, name: upvotedPlace.name });
   }
 
 }
