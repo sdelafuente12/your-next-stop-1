78b39e6b3577e4ea0142b92a34f82ed971ef2181
gmarinojr <garymarinojr@gmail.com>
2019-10-14 12:05:37 -0500

    removed map for now. not working.

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Observable, from } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router'
-import { map, take, distinct } from 'rxjs/operators';
-import { MapComponent } from '../map/map.component';
+import { map, take } from 'rxjs/operators';
 
 @Component({
   selector: 'app-details',
@@ -10,41 +9,23 @@ import { MapComponent } from '../map/map.component';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent implements OnInit {
-  @ViewChild(MapComponent, { static: false }) private map: MapComponent;
   newColor = false;
   state$: Observable<object>;
   placeId: string;
-  placesSubscription;
-  imagesSubscription;
-  public places = [];
-  public images = [];
 
-  constructor(public activatedRoute: ActivatedRoute) {}
-
-  ngOnInit() {
-    this.state$ = this.activatedRoute.paramMap.pipe(
-      map(value => (this.placeId = window.history.state)),
-      take(1)
-    );
-    this.state$.subscribe(state => console.log('state', state));
-  }
-
-  loadPlaces() {
-    this.placesSubscription = from(this.map.nearbyPlaces).subscribe(place => {
-      this.places.push(place);
-    });
-  }
-
-  loadImages() {
-    this.imagesSubscription = from(this.map.images)
-      .pipe(distinct())
-      .subscribe(image => {
-        this.images.push(image);
-      });
-  }
-
+  constructor(public activatedRoute: ActivatedRoute) { }
+  
   toggleColor() {
     this.newColor = !this.newColor;
-    console.log('color change');
+    console.log('color change')
   }
+  ngOnInit() {
+    this.state$ = this.activatedRoute.paramMap
+      .pipe(
+        map((value) => this.placeId = window.history.state),
+        take(1)
+        )
+    this.state$.subscribe(state => console.log('state', state))
+  }
+
 }
