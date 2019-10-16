0c1068f933fd5193a8f71e38525b2b027ec0040b
gmarinojr <garymarinojr@gmail.com>
2019-10-16 11:17:22 -0500

    commented out console.logs

--- src/app/trips/trips.component.ts
+++ src/app/trips/trips.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { TripsService } from '../services/trips.service';
-// import { IgxTextHighlightDirective, IgxCardThumbnailDirective } from 'igniteui-angular';
 
 @Component({
   selector: 'app-trips',
@@ -12,30 +11,17 @@ export class TripsComponent implements OnInit {
   public upcoming = [];
   public current = [];
   public previous = [];
-  // public current = [
-  //   {
-  //     origin: 'New Orleans, LA',
-  //     destination: 'Atlanta, GA'
-  //   }
-  // ];
-  // public previous = [
-  //   {
-  //     origin: 'New Orleans, LA',
-  //     destination: 'New York, NY'
-  //   }
-  // ];
 
   constructor(private trips: TripsService) {}
   
   ngOnInit() {
-    console.log('HIIIIIIIIIIIII');
     this.getAllTrips();
   }
 
   getAllTrips() {
     return this.trips.getAllTrips(this.currentUser)
     .subscribe((response: Object[]): void => {
-      console.log('USERS TRIPS RESPONSE', response);
+      // console.log('USERS TRIPS RESPONSE', response);
       response.forEach(element => {
         if(element[0].status === 'current'){
           this.current.push(element)
@@ -45,13 +31,12 @@ export class TripsComponent implements OnInit {
           this.upcoming.push(element)
         }
       });
-      console.log('UPCOMING', this.upcoming,'CURRENT', this.current, 'PREVIOUS', this.previous);    
+      // console.log('UPCOMING', this.upcoming,'CURRENT', this.current, 'PREVIOUS', this.previous);    
     })
   }
   onSelection(trip){
     console.log(trip)
     let storageTrip = JSON.stringify(trip)
     localStorage.setItem("trip", storageTrip);
-    //return 'boob'
   }
 }
